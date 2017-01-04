e3aa95b5af153ab259f066e9ceb9e74b983ceab8
gib98 <gibran.essa@gmail.com>
2017-01-04 17:41:49 -0500

    messed with error handling, and formatted better

--- music.py
+++ music.py
@@ -4,33 +4,39 @@ import asyncio
 import glob
 from pygame import mixer
 
-def gotoEnsureDir(f):
+
+def goto_ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
     os.chdir(d)
-    
-def downloadAudio(youtubeURL):
+
+
+def download_audio(youtubeURL):
     urlStart='http://www.youtubeinmp3.com/fetch/?video='
     url = urlStart+youtubeURL
     fileName = youtubeURL[youtubeURL.index('=')+1:]+'.mp3'
+
     urllib.request.urlretrieve(url, fileName)
+
     return fileName
-mixer.init()
-gotoEnsureDir('music/')
-playlist = []
+
 
 def dl():
-    playlist.append(downloadAudio('https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
-    playlist.append(downloadAudio('https://www.youtube.com/watch?v=ENXvZ9YRjbo'))
+    playlist.append(download_audio('https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
+    playlist.append(download_audio('https://www.youtube.com/watch?v=ENXvZ9YRjbo'))
 
-    
 
-def playPlaylist():
+def play_playlist():
     mixer.music.load(playlist[0])
     mixer.music.play()
     mixer.music.set_endevent()
 
+
 def clear():
     for item in glob.glob('*'):
         os.remove(item)
+
+mixer.init()
+goto_ensure_dir('music/')
+playlist = []
