91514e74bf85d6114c8a0c613ee180930a4e00d0
gib98 <gibran.essa@gmail.com>
2017-01-09 15:06:09 +0000

    tried to get pygame music player working (and failed)

--- music.py
+++ music.py
@@ -1,20 +1,21 @@
 import urllib.request
 import os
-import asyncio
 import glob
-from pygame import mixer, event
+from pygame import mixer, event, USEREVENT, init
 
 def goto_ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
     os.chdir(d)
+goto_ensure_dir('music/')
+playlist = []
+queue = 0
 
-
-def download_audio(youtube_url):
-    url_start = 'http://www.youtubeinmp3.com/fetch/?video='
-    url = url_start + youtube_url
-    file_name = youtube_url[youtube_url.index('=') + 1:] + '.mp3'
+def download_audio(youtube_url_id):
+    url_start = 'http://www.youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v='
+    url = url_start + youtube_url_id
+    file_name = youtube_url_id + '.mp3'
 
     urllib.request.urlretrieve(url, file_name)
 
@@ -25,15 +26,24 @@ def dl():
     playlist.append(download_audio('http://www.youtube.com/watch?v=btNs5wxTJ2k'))
     playlist.append(download_audio('https://www.youtube.com/watch?v=ENXvZ9YRjbo'))
 
+def add_song(file_name):
+    playlist.append(file_name)
+
+def play_next_song():
+    global playlist
+    global queue
+    mixer.music.load(playlist[queue])
+    mixer.music.play()
+    mixer.music.set_endevent(USEREVENT+1)
+    queue += 1
 
 def play_playlist():
-    mixer.music.load(playlist[0])
-    mixer.music.play()
-    mixer.music.set_endevent(SONG_END)
+    play_next_song()
+    
     while True:
-        for event in event.get():
-            break
-        break
+        for act in event.get():
+            if act.type == USEREVENT+1:  
+                play_next_song()
         
         
     
@@ -44,7 +54,6 @@ def clear():
         os.remove(item)
 
 
-goto_ensure_dir('music/')
-playlist = []
+init()
 
 mixer.init()
